b67f83a7a25eeef4edec034936afbbee43bb5be4
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2016-12-29 14:39:45 +0100

    Started work on creating a LMNL parser using ANTLR and the lexicon created yesterday.

---
+++ alexandria/lmnl-grammar/src/LMNLParser.g4
@@ -0,0 +1,13 @@
+/* LMNL Parser grammar
+ * @author: Ronald Haentjens Dekker
+ *
+ *
+ */
+
+parser grammar LMNLParser;
+
+options { tokenVocab=LMNLGrammar; }
+
+document    :   TEXT;
+
+
---
+++ alexandria/lmnl-grammar/src/TestTheLMNLParser.java
@@ -0,0 +1,51 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/**
+ * Created by Ronald Haentjens Dekker on 29/12/16.
+ */
+public class TestTheLMNLParser {
+
+    public void testParserTextOnly() {
+        String input = "He manages to keep the upper hand";
+        LMNLParser.DocumentContext documentContext = setupParserAndReturnParseTree(input);
+        String stringTree = documentContext.toStringTree();
+        System.out.println(stringTree);
+
+
+    }
+
+
+
+    public void testParserComplexExample() {
+        String input = "[excerpt\n" +
+                "  [source [date}1915{][title}The Housekeeper{]]\n" +
+                "  [author\n" +
+                "    [name}Robert Frost{]\n" +
+                "    [dates}1874-1963{]] }\n" +
+                "[s}[l [n}144{n]}He manages to keep the upper hand{l]\n" +
+                "[l [n}145{n]}On his own farm.{s] [s}He's boss.{s] [s}But as to hens:{l]\n" +
+                "[l [n}146{n]}We fence our flowers in and the hens range.{l]{s]\n" +
+                "{excerpt]";
+        setupParserAndReturnParseTree(input);
+
+    }
+
+    private LMNLParser.DocumentContext setupParserAndReturnParseTree(String input) {
+        LMNLGrammar lexer = new LMNLGrammar(new ANTLRInputStream(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LMNLParser parser = new LMNLParser(tokens);
+        parser.setBuildParseTree(true);
+        // the method is called like the first rule in the parser grammar, in this case: document
+        LMNLParser.DocumentContext document = parser.document();
+        return document;
+    }
+
+
+    public static void main(String args[]) {
+        TestTheLMNLParser test = new TestTheLMNLParser();
+        test.testParserTextOnly();
+    }
+
+
+}
