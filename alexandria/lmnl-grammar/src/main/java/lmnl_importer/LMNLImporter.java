d1f8a4595b5b621c7263cb787837259bbe364ffd
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2017-01-20 18:06:54 +0100

    Mavenized the project. Disabled the LMNL parser for the moment. I am thinking of using only the lexer for the moment.

--- alexandria/lmnl-grammar/src/main/java/lmnl_importer/LMNLImporter.java
+++ alexandria/lmnl-grammar/src/main/java/lmnl_importer/LMNLImporter.java
@@ -2,9 +2,7 @@ package lmnl_importer;
 
 import data_model.Document;
 import lmnl_antlr.LMNLLexer;
-import lmnl_antlr.LMNLParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
 
 /**
  * Created by Ronald Haentjens Dekker on 29/12/16.
@@ -13,15 +11,16 @@ public class LMNLImporter {
 
     public Document importLMNL(String input) {
         LMNLLexer lexer = new LMNLLexer(new ANTLRInputStream(input));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        LMNLParser parser = new LMNLParser(tokens);
-        parser.setBuildParseTree(true);
-        // the method is called like the first rule in the parser grammar, in this case: document
-        LMNLParser.DocumentContext document = parser.document();
-        LMNLImporterVisitor v = new LMNLImporterVisitor();
-        document.accept(v);
-        Document text = v.getResult();
-        return text;
+        return null;
+        //        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        LMNLParser parser = new LMNLParser(tokens);
+//        parser.setBuildParseTree(true);
+//        // the method is called like the first rule in the parser grammar, in this case: document
+//        LMNLParser.DocumentContext document = parser.document();
+//        LMNLImporterVisitor v = new LMNLImporterVisitor();
+//        document.accept(v);
+//        Document text = v.getResult();
+//        return text;
     }
 
 }
